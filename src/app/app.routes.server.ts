6c7eb47d12eaeebaf893b5cf5ed390478dfb2d42
unknown <amir_badiei@yahoo.com>
2026-06-23 13:57:28 +0330

    Output mode removed

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,8 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'gold-calculator', renderMode: RenderMode.Server },
+  { path: 'converter', renderMode: RenderMode.Server },
   {
     path: ':group/:title',
     renderMode: RenderMode.Server
