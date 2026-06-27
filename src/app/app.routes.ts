2b3ed35131a0cf29bc25b74bc88a5c07c7b06afe
unknown <amir_badiei@yahoo.com>
2026-06-27 12:58:42 +0330

    Lazy loading removed for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { GoldCalculatorComponent } from './pages/gold-calculator/gold-calculator.component';
+import { ConverterComponent } from './pages/converter/converter.component';
+import { CurrencyItemDetailsComponent } from './pages/currency-item-details/currency-item-details.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', component: HomeComponent, title: 'ارزیاب | مرجع قیمت بازارها' },
     { path: 'gold-calculator', component: GoldCalculatorComponent, title: 'ارزیاب | محاسبه‌گر طلا' },
-    { path: 'converter', loadComponent: () => import('../app/pages/converter/converter.component').then((converter) => converter.ConverterComponent), title: 'ارزیاب | مبدل ارز' },
-    { path: ':group/:title', loadComponent: () => import('../app/pages/currency-item-details/currency-item-details.component').then((item) => item.CurrencyItemDetailsComponent) },
-    { path: '**', loadComponent: () => import('../app/pages/not-found/not-found.component').then((notFound) => notFound.NotFoundComponent), title: 'ارزیاب | صفحه مورد نظر یافت نشد' },
+    { path: 'converter', component: ConverterComponent, title: 'ارزیاب | مبدل ارز' },
+    { path: ':group/:title', component: CurrencyItemDetailsComponent },
+    { path: '**', component: NotFoundComponent, title: 'ارزیاب | صفحه مورد نظر یافت نشد' },
 ];
