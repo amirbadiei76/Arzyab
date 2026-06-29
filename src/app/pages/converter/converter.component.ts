bedd5bd569484f917334c400d796d6076b2cb53c
unknown <amir_badiei@yahoo.com>
2026-06-29 19:15:38 +0330

    Some tests added

--- src/app/pages/converter/converter.component.ts
+++ src/app/pages/converter/converter.component.ts
@@ -185,7 +185,10 @@ export class ConverterComponent {
     this.requestArray.cryptoList,
     this.requestArray.mainData!.pipe(filter(data => !!data?.current))
   ]).pipe(
-    filter(([mainList, cryptoList]) => mainList.length > 0 && cryptoList.length > 0),
+    filter(([mainList, cryptoList]) => {
+      console.log(mainList, cryptoList)
+      return mainList.length > 0 && cryptoList.length > 0
+    }),
     take(1),
     shareReplay(1)
   );
@@ -226,13 +229,19 @@ export class ConverterComponent {
 
   
   fromItem$ = combineLatest([this.dualList$, toObservable(this.fromItemId)]).pipe(
-    map(([{ first }, id]) => first.find(item => item.id === id)),
+    map(([{ first }, id]) => {
+      console.log(first, id, first.find(item => item.id === id))
+      return first.find(item => item.id === id)
+    }),
     shareReplay(1)
   );
   fromItem = toSignal(this.fromItem$);
 
   toItem$ = combineLatest([this.dualList$, toObservable(this.toItemId)]).pipe(
-    map(([{ second }, id]) => second.find(item => item.id === id)),
+    map(([{ second }, id]) => {
+      console.log(second, id, second.find(item => item.id === id))
+      return second.find(item => item.id === id)
+    }),
     shareReplay(1)
   );
   toItem = toSignal(this.toItem$);
@@ -303,10 +312,10 @@ export class ConverterComponent {
 
       const defaultFrom = first?.[1];
       const defaultTo = second?.[this.currencyType() === 2 ? 1 : 0];
-
+      console.log(defaultFrom, defaultTo)
       const matchedFrom = first.find(i => i.slugText === fromSlug) ?? defaultFrom;
       const matchedTo = second.find(i => i.slugText === toSlug) ?? defaultTo;
-
+      console.log(matchedFrom, matchedTo)
       if (matchedFrom) this.fromItemId.set(matchedFrom.id);
       if (matchedTo) this.toItemId.set(matchedTo.id);
 
@@ -321,7 +330,8 @@ export class ConverterComponent {
       this.dualList$.pipe(take(1)).subscribe(({ first, second }) => {
         const defaultFrom = first?.[1];
         const defaultTo = second?.[this.currencyType() === 2 ? 1 : 0];
-
+        
+        console.log(defaultFrom, defaultTo)
         this.fromItemId.set(defaultFrom?.id ?? '');
         this.toItemId.set(defaultTo?.id ?? '');
       });
